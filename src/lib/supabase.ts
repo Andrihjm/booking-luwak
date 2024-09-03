1fee46ea13d6b9967facf76c84f96136c3c7763b
andri <andri@pop-os.localdomain>
2024-09-03 13:22:46 +0700

    setup upload image

---
+++ src/lib/supabase.ts
@@ -0,0 +1,29 @@
+import { createClient } from "@supabase/supabase-js";
+
+const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!!;
+const NEXT_PUBLIC_SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY!!;
+
+export const supabase = createClient(
+  NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_KEY
+);
+
+export const uploadFile = async (file: File) => {
+  try {
+    const fileName = `${Date.now()}.png`;
+
+    const { data, error } = await supabase.storage
+      .from("ImageUpload")
+      .upload(`public/airplane/${fileName}`, file, {
+        cacheControl: "3600",
+        upsert: false,
+      });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
